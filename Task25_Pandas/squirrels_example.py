89ec55cb015d1161341cbf63e74ac9aace9b6e65
Anna <annaf111@gmail.com>
2025-03-30 13:49:05 +0100

    [TASK25] Pandas Library

---
+++ Task25_Pandas/squirrels_example.py
@@ -0,0 +1,25 @@
+import pandas
+
+data = pandas.read.csv("central_park_squirrel.csv")
+
+colors_dict = {
+    "color": [],
+    "counter": []
+}
+
+# Print how many squirrels are grey
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+
+# Check how many are from each color
+for color in data.PrimaryFurColor:
+    found_color = False
+    for saved_color in colors_dict["color"]:
+        if saved_color == color:
+            colors_dict["counter"] += 1
+            found_color = True
+    if not found_color:
+        colors_dict["color"].append(color)
+        colors_dict["counter"].append(1)
+
+df = pandas.DataFrame(colors_dict)
+df.to_csv("Squirrel_count.csv")
---
+++ Task25_Pandas/weather_example.py
@@ -0,0 +1,36 @@
+import csv
+import pandas
+
+# Writing csv files
+with open("weather.csv") as data_file:
+    data = csv.reader(data_file)
+    all_temperatures = []
+    for row in data:
+        print(row)
+
+# Using pandas is much simpler
+print(data["temp"])
+# Check the type of data, series is a column - like a list
+type(data["temp"])
+# Use data as a dictionary
+data_dict = data.to_dict()
+print(data_dict)
+
+# Change data to a list
+temp_list = data["temp"].to_list()
+# It can be used the first line as name for the list without declaring it
+temp_list2 = data.temp
+
+# Save day of higher temp
+for weather in data:
+    if weather.temp == weather.temp.max:
+        day_temp_high = weather.temp
+
+
+# Save data in a csv file
+data_dict = {
+    "students": ["Amy", "James", "Angela"]
+    "scores": [115, 32, 22]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
