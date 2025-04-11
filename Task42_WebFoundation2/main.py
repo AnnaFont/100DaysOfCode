b0b545f3a72437503933c9999caf153273b0e553
Anna <annaf111@gmail.com>
2025-04-11 20:27:26 +0100

    [Task_42] Web Foundation 2

---
+++ Task42_WebFoundation2/main.py
@@ -0,0 +1,22 @@
+# If you write ! and enter it will put the starting lines automatically
+
+
+### LISTS
+
+# Ul - unordered list and ol - ordered list. The unordered use bullet points and the ordered use numbers in each line
+# Each list li - list items (it is needed 1 per each element of the list) - Check index.html example
+# It is possible to add lists inside other lists
+# Right click in the file and click on Show preview so you will see how it is looking 
+# <ol start="5"> start element will be 5.
+
+### Attributes
+
+# Attributes: <tag attribute1=value1 attribute2=value2>
+# Anchor elements, href link: <a href="http://www.google.com">This is a link<a/>
+# draggable=true, you can move the element around
+
+### Images
+
+#<img src="https//picsum.photos/200" alt="image name"/>
+#alt means alternative text description
+#src can be a website or an image in the project
