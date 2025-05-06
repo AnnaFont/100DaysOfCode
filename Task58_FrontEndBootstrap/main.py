951b64631bd9f6beed53ad7dc328cecd7fcc447e
Anna <annaf111@gmail.com>
2025-05-06 13:08:37 +0100

    [Task_58] Bootstrap Front End

---
+++ Task58_FrontEndBootstrap/main.py
@@ -0,0 +1,9 @@
+# Bootstrap is a type of CSS Framework (see index.hml doc
+# https://getbootstrap.com/
+# It can be search in internet free bootstrap components and copy the part you need using the inspect
+# It is needed to add a couple of lines i the html:
+# 1.  Add in the head:
+# <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+#     integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+# 2. Add at the end of the body:
+# <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
