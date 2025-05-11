a83156ebb4e3eb4ab03943e89e13e00eb83549fd
Anna <annaf111@gmail.com>
2025-05-11 20:54:43 +0100

    [Task_66] API Cafe

---
+++ Task65_WebDesignExplanation/main.py
@@ -0,0 +1,41 @@
+# Color theory
+# Yellow -> Attention
+# Green -> Fresh, safety (for food companies)
+# Blue -> Stability, trust, serenity (for financial companies)
+# Purple -> Royalty, wealth
+
+# Analogous colors -> Two similar colors (for text
+# Complementary Colors -> Two really diferent to take your attention, pop-up, freshness - Do not use it for text
+
+# Colorhunt.co -> Combination of colors
+
+# Typography
+# Fonts:
+# Serif typefaces - Inspired by comics.
+# It has old and modern. It has Sans-Serif for startups, modern and when there is a lot of text
+
+# To be used 2 fonts in 1 design only
+
+# Colours
+# Different colours for importance and hierarchy
+
+# Layout
+# Different shapes (wikipedia is not good for it)
+# $0 characters for line, not two long
+
+# Alignment
+# Eg use title in the left and text aligned
+
+# Inject whitespace between the products to make it seem more expensive, minimalist
+
+# Design for your audience
+
+
+# User Experience Design (UX Design)
+# Go to one part of the company to another -> Reading patterns
+# F-Layout - Read from left to right and down
+# Z-Layout - Faster
+
+# All platform design - for all kind of screens
+
+
---
+++ Task66_BuildAPI/explanation.py
@@ -0,0 +1,4 @@
+# REST: Architect style to make an API
+# Error 404: Request not found
+# Client to Server - HTTP Request (or HTTPS to encrypt the request and make it secure)
+# GET (fetch) / POST (create) / PUT (update) / PATCH(update) / DELETE
